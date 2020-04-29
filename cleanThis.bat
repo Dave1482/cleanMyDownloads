89f2936f602107a707a2e28e68a32c4f42d43e09
Dave T <thedave1482@gmail.com>
2020-04-29 12:13:13 -0500

    Create cleanThis.bat

---
+++ cleanThis.bat
@@ -0,0 +1,27 @@
+@echo off
+set me=%USERPROFILE%
+move %me%\Downloads\*.7z %me%\Downloads\Archives\
+move %me%\Downloads\*.avi %me%\Videos\
+move %me%\Downloads\*.bmp %me%\Pictures\
+move %me%\Downloads\*.bz2 %me%\Downloads\Archives\
+move %me%\Downloads\*.csv %me%\Documents\Downloads\Sheets\
+move %me%\Downloads\*.deb %me%\Downloads\Archives\
+move %me%\Downloads\*.dmg %me%\Downloads\Archives\Images\
+move %me%\Downloads\*.doc %me%\Documents\Downloads\Docs\
+move %me%\Downloads\*.docx %me%\Documents\Downloads\Docs\
+move %me%\Downloads\*.exe %me%\Downloads\EXEs\
+move %me%\Downloads\*.gz %me%\Downloads\Archives\
+move %me%\Downloads\*.ico %me%\Pictures\
+move %me%\Downloads\*.img %me%\Downloads\Archives\Images\
+move %me%\Downloads\*.iso %me%\Downloads\Archives\Images\
+move %me%\Downloads\*.jpg %me%\Pictures\
+move %me%\Downloads\*.mov %me%\Videos\
+move %me%\Downloads\*.mp4 %me%\Videos\
+move %me%\Downloads\*.pdf %me%\Documents\Downloads\Docs\
+move %me%\Downloads\*.plist %me%\Documents\Downloads\Programming\
+move %me%\Downloads\*.png %me%\Pictures\
+move %me%\Downloads\*.txt %me%\Documents\Downloads\Text\
+move %me%\Downloads\*.xlsx %me%\Documents\Downloads\Sheets\
+move %me%\Downloads\*.xlsb %me%\Documents\Downloads\Sheets\
+move %me%\Downloads\*.xml %me%\Documents\Downloads\Programming\
+move %me%\Downloads\*.zip %me%\Downloads\Archives\
