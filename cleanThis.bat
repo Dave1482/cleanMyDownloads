e197c61540a0231f5f5c741b1b0c9bc7f2246a17
Dave T <thedave1482@gmail.com>
2020-04-30 10:36:29 -0500

    Update cleanThis.bat

--- cleanThis.bat
+++ cleanThis.bat
@@ -1,5 +1,14 @@
 @echo off
 set me=%USERPROFILE%
+
+IF NOT exist %me%\Downloads\Archives\ ( mkdir %me%\Downloads\Archives\ && echo %me%\Downloads\Archives\ created)
+IF NOT exist %me%\Downloads\Archives\Images\ ( mkdir %me%\Downloads\Archives\Images\ && echo %me%\Downloads\Archives\Images\ created)
+IF NOT exist %me%\Documents\Downloads\Docs\ ( mkdir %me%\Documents\Downloads\Docs\ && echo %me%\Documents\Downloads\Docs\ created)
+IF NOT exist %me%\Documents\Downloads\Programming\ ( mkdir %me%\Documents\Downloads\Programming\ && echo %me%\Documents\Downloads\Programming\ created)
+IF NOT exist %me%\Documents\Downloads\Sheets\ ( mkdir %me%\Documents\Downloads\Sheets\ && echo %me%\Documents\Downloads\Sheets\ created)
+IF NOT exist %me%\Documents\Downloads\Text\ ( mkdir %me%\Documents\Downloads\Text\ && echo %me%\Documents\Downloads\Text\ created)
+IF NOT exist %me%\Downloads\EXEs\ ( mkdir %me%\Downloads\EXEs\ && echo %me%\Downloads\EXEs\ created)
+
 move %me%\Downloads\*.7z %me%\Downloads\Archives\
 move %me%\Downloads\*.avi %me%\Videos\
 move %me%\Downloads\*.bmp %me%\Pictures\
